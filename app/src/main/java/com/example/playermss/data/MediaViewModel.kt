3c5086fcfa5e299af2eafd692a62a4b5edf115ba
v <modseller@mail.ru>
2025-02-24 17:37:39 +0300

    begin move mediacontroller to viewmodel

--- app/src/main/java/com/example/playermss/data/MediaViewModel.kt
+++ app/src/main/java/com/example/playermss/data/MediaViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.playermss.data
 
+import android.content.ComponentName
 import android.content.Context
 import android.database.Cursor
 import android.os.Build
@@ -8,6 +9,16 @@ import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.media3.common.MediaMetadata
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
+import androidx.media3.datasource.HttpDataSource
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.example.playermss.PlaybackService
+import com.example.playermss.TrackList
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.common.util.concurrent.MoreExecutors
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -62,6 +73,57 @@ class MediaViewModel: ViewModel() {
     val expandedAlbums: StateFlow<Set<String>> = _expandedAlbums.asStateFlow()
 
     lateinit var context: Context
+
+
+    private var controllerFuture: ListenableFuture<MediaController>? = null
+    private var mediaController: MediaController? = null
+
+    fun init(){
+        val sessionToken =
+            SessionToken(
+                context,
+                ComponentName(context, PlaybackService::class.java)
+            )
+        controllerFuture =
+            MediaController.Builder(context, sessionToken).buildAsync()
+        controllerFuture?.addListener({
+            // MediaController is available here with controllerFuture.get()
+            mediaController = controllerFuture?.get()
+
+//            visualizer = Visualizer(sessionToken.uid)
+//            bassBoost = BassBoost(0,sessionToken.uid)
+
+            mediaController?.addListener(
+                object : Player.Listener {
+
+                    override fun onPlayerError(error: PlaybackException) {
+                        val cause = error.cause
+                        if (cause is HttpDataSource.HttpDataSourceException) {
+                            // An HTTP error occurred.
+                            val httpError = cause
+                            // It's possible to find out more about the error both by casting and by querying
+                            // the cause.
+                            if (httpError is HttpDataSource.InvalidResponseCodeException) {
+                                // Cast to InvalidResponseCodeException and retrieve the response code, message
+                                // and headers.
+                            } else {
+                                // Try calling httpError.getCause() to retrieve the underlying cause, although
+                                // note that it may be null.
+                            }
+                        }
+                    }
+
+                    override fun onMediaMetadataChanged(mediaMetadata: MediaMetadata) {
+                        Log.d("DMG", "mediaData changed")
+                    }
+                }
+            )
+
+//            mediaControllerLoaded.value = true
+
+        }, MoreExecutors.directExecutor())
+
+    }
 
 
     private fun toggleInSet(inSet: Set<String>, name: String) : Set<String>{
