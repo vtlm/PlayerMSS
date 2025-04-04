3d443c4610ff24b6d0fb7e0ed3ee0f5d50f2210f
v <modseller@mail.ru>
2025-04-04 13:31:41 +0300

    pending intent to return to app from notification

--- app/src/main/java/com/example/playermss/PlaybackService.kt
+++ app/src/main/java/com/example/playermss/PlaybackService.kt
@@ -1,13 +1,18 @@
 package com.example.playermss
 
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.os.Handler
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.OptIn
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
 import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.DefaultMediaNotificationProvider
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 
@@ -27,8 +32,11 @@ class PlaybackService : MediaSessionService() {
             .build()
         player?.setAudioAttributes(audioAttributes,true)
 
-         mediaSession = MediaSession.Builder(this, player!!).build()
+         mediaSession = MediaSession.Builder(this, player!!)
+             .setSessionActivity(getSessionActivityIntent())
+             .build()
          Log.d("OC","Playback service created")
+
 
 //        val mainHandler = Handler(mainLooper)
 //        mainHandler.post { // Do your stuff here related to UI, e.g. show toast
@@ -75,4 +83,14 @@ class PlaybackService : MediaSessionService() {
         Log.d("MSS","destroyed")
 
     }
+}
+
+@OptIn(UnstableApi::class)
+private fun Context.getSessionActivityIntent(): PendingIntent {
+    return PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java),
+        PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+    )
 }
