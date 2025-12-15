6fc325812e5715e622fe82f2c7ff8553153f8e2a
v <modseller@mail.ru>
2025-12-15 13:57:24 +0300

    + auto complete query fields, base on user input history

--- app/src/main/java/com/example/playermss/SearchFields.kt
+++ app/src/main/java/com/example/playermss/SearchFields.kt
@@ -8,22 +8,33 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import com.example.playermss.data.QueryTextField
 
 @RequiresApi(Build.VERSION_CODES.Q)
 @Composable
-fun SearchField(textValue: QueryTextField, onDone:  () -> Unit? = {}){
+fun SearchField1(textValue: QueryTextField, onDone:  () -> Unit? = {}){
     val text = textValue.text.collectAsState()
     val keyboardController = LocalSoftwareKeyboardController.current
     val textField = FocusRequester()
@@ -31,7 +42,8 @@ fun SearchField(textValue: QueryTextField, onDone:  () -> Unit? = {}){
     OutlinedTextField(
         value = text.value,
         singleLine = true,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .focusRequester(textField),
         onValueChange = {str -> textValue.setText(str)},
         label = { Text(textValue.description) },
@@ -58,6 +70,84 @@ fun SearchField(textValue: QueryTextField, onDone:  () -> Unit? = {}){
                 onDone()
             }
         ))
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@RequiresApi(Build.VERSION_CODES.Q)
+@Composable
+fun SearchField(queryTextField: QueryTextField, onDone:  () -> Unit? = {}){
+    var expanded by remember { mutableStateOf(false) }
+
+    val text = queryTextField.text.collectAsState()
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val textField = FocusRequester()
+
+    ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = { expanded = it }) {
+        OutlinedTextField(
+            value = text.value,
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(textField)
+                .menuAnchor(MenuAnchorType.PrimaryEditable)
+                //Set this modifier to dismiss the menu when the text field is not in focus,
+                //e.g. moving to another text field by using keyboard actions.
+                .onFocusChanged { focusState ->
+                    if (!focusState.isFocused) {
+                            expanded = false
+                    }
+                },
+
+            onValueChange = { str ->
+                queryTextField.setText(str)
+                expanded = queryTextField.userInputDictIsNotEmpty()
+            },
+            label = { Text(queryTextField.description) },
+            trailingIcon = {
+                if (text.value != "") {
+                    IconButton(onClick = {
+                        queryTextField.setText("")
+                        textField.requestFocus()
+                        if (queryTextField.userInputDictIsNotEmpty()) {
+                            expanded = true
+                        }
+                    }) {
+                        Icon(
+                            Icons.Default.Clear,
+                            contentDescription = "Clear"
+                        )
+                    }
+                }
+            },
+            isError = false,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    keyboardController?.hide()
+                    queryTextField.updateUserInputDict()
+                    onDone()
+                }
+            ))
+
+        if(queryTextField.userInputDictFilteredByCurrentText().isNotEmpty()) {
+
+            ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                queryTextField.userInputDictFilteredByCurrentText().forEach { option ->
+                    DropdownMenuItem(
+                        text = { Text(option, style = MaterialTheme.typography.bodyLarge) },
+                        onClick = {
+                            queryTextField.setText(option)
+                            expanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                    )
+                }
+            }
+        }
+
+
+    }
 }
 
 @RequiresApi(Build.VERSION_CODES.Q)
--- app/src/main/java/com/example/playermss/data/QueryTextField.kt
+++ app/src/main/java/com/example/playermss/data/QueryTextField.kt
@@ -1,6 +1,7 @@
 package com.example.playermss.data
 
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.stringSetPreferencesKey
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,13 +13,42 @@ class QueryTextField (
     private val launchScope: CoroutineScope
 ){
     private val key = stringPreferencesKey(description)
+    private val userInputDictKey = stringSetPreferencesKey(description+"UserDict")
     private val savedText: StateFlow<String> = userPreferencesRepository.getOrDefaultAsStateFlow(key,launchScope,"")
+    private val savedUserInputDict: StateFlow<Set<String>> = userPreferencesRepository.getOrDefaultAsStateFlow(userInputDictKey,launchScope,setOf())
     private val _text = MutableStateFlow(savedText.value)
     val text: StateFlow<String> = _text.asStateFlow()
+    private val _userInputDict = MutableStateFlow(savedUserInputDict.value)
+    val userInputDict: StateFlow<Set<String>> = _userInputDict.asStateFlow()
 
     fun setText(text: String){
         _text.value = text
     }
 
-    fun saveText() = userPreferencesRepository.set(key, launchScope, _text.value)
+    fun userInputDictIsNotEmpty(): Boolean {
+        return userInputDict.value.any { it.isNotBlank() }
+    }
+
+    fun userInputDictFilteredByCurrentText():List<String>{
+        return userInputDict.value
+            .filter { it.contains(text.value) }
+            .filter { it.isNotBlank() }
+            .sortedBy { it }
+    }
+
+    fun saveText() {
+        userPreferencesRepository.set(key, launchScope, _text.value)
+        updateUserInputDict()
+        saveUserInputDict()
+    }
+
+    fun addToUserInputDict(text: String){
+        _userInputDict.value += text
+    }
+
+    fun updateUserInputDict(){
+        addToUserInputDict(_text.value)
+    }
+
+    fun saveUserInputDict() = userPreferencesRepository.set(userInputDictKey, launchScope,userInputDict.value)
 }
